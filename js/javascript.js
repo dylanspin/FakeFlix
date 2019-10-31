5d1523ecfda77fb807b34ddfaf7ddb5b090c7e19
dylan <dylanspin100@hotmail.com>
2019-10-31 14:08:37 +0100

    update
    bijna klaar heb nu de sliders ook heb ik 3 verschillende acount pagina's
    de responsive voor de iphone is ook goed

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,15 @@
 
 function page(loc){
-  window.location.href = loc+".html";
+  if(loc == 10){
+    window.location.href = "mylist.html";
+    console.log("tien");
+  }
+  else{
+    console.log("test");
+    window.location.href = loc+".html";
+  }
 }
+
 function check(){
   var y = window.scrollY;
   if(y <= 270){
@@ -12,45 +20,24 @@ function check(){
   }
 }
 
+function profiel(){
+  var path = window.location.pathname;
+  var page = path.split("/").pop().replace('.html','');
+  document.getElementById('profile').innerHTML = "<img src='pic/prof"+page+".png' class='image' onclick='schuif(2,46)'>";
+}
+
 function random(){
   var films = ["","Forrest Gump","Live Die repeat","Hacksaw Ridge","Joker","Ted","King Arthur","LEGEND",
                "Ted 2","A million ways to die in the wild west","Ready Player one","Why Him","Vacation"
                ,"We are the Millers","Fury","Mad Max","Advengers End game","Spider man 2","Spider man",
                "Deadpool","night at the museum","The wolf of wall street","Dunkirk","Martian","Venom",
-               "Ant-man","Grown ups 2","Grown ups 1","War dogs","Pirates of the carabian","Pirates of the carabian"]
-    var random = Math.floor(Math.random() * 30+1);
+               "Ant-man","Grown ups 2","Grown ups 1","War dogs","Pirates of the carabian","Pirates of the carabian",
+                "Jhony English 2","Jhony English","22 Jump Street","23 Jump Street","Baby driver","Green book","Inglorius bastards",
+               "Transcendence","Burnt","White boy Ricky","Unbroken","Hansel and Gretal","U.N.C.L.E","Non-Stop","Idiocrecy","Click",
+                ]
+    var random = Math.floor(Math.random() * 46+1);
     document.getElementById('naam').innerHTML = films[random];
     document.getElementById('movie').innerHTML = "<img src='pic/m"+random+".jpg' class='film'>";
-}
-
-var af = [0,0,0,0,0,0,0,0];
-var tell = [3,3,3,3,3,3,3,3];
-
-function next(t){
-  if(tell[t] >= 0){
-    af[t]-=2728;
-    tell[t] --;
-    document.getElementById("links"+t).style.visibility = "visible";
-    document.getElementById("slide"+t).style.marginLeft = af[t]+"px";
-    if(tell[t] == -1){
-      document.getElementById("rechts"+t).style.visibility = "hidden";
-    }
-  }
-}
-
-function back(t){
-  if(tell[t] >= -1 && tell[t] <= 2 ){
-    af[t]+=2728;
-    tell[t] ++;
-    document.getElementById("rechts"+t).style.visibility = "visible";
-    document.getElementById("slide"+t).style.marginLeft = af[t]+"px";
-    if(tell[t] == 3){
-      document.getElementById("links"+t).style.visibility = "hidden";
-    }
-  }
-  else{
-    document.getElementById("links"+t).style.visibility = "hidden";
-  }
 }
 
 var checktrue = [true,true,true];
